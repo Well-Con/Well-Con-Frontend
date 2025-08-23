ee9c160fd79e4c512fa9cb8e36a3de026fde2301
Neeraj <neerajkaushik439@gmail.com>
2025-08-23 23:09:08 +0530

    fixed sidebar

--- src/pages/doctor/contains/Sidebar.jsx
+++ src/pages/doctor/contains/Sidebar.jsx
@@ -1,11 +1,148 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
+// import { useLocation } from "react-router-dom";
+// import '../../../styles/Sidebar.scss'
+
+// const Sidebar = () => {
+//     const location = useLocation();
+
+//     const [closeMenu, setCloseMenu] = useState(false);
+
+//     const handleCloseMenu = () => {
+//         setCloseMenu(!closeMenu);
+//     };
+
+//     return (
+//         <div className={closeMenu === false ? "sidebar w-64" : "sidebar active"}>
+//             {/* logo */}
+//             <div
+//                 className={
+//                     closeMenu === false
+//                         ? "logoContainer"
+//                         : "logoContainer active"
+//                 }
+//             >
+
+//                 <i className="fa-brands fa-slack text-green-500 fa-lg logo "></i>
+//                 <h2 className="title ">Well-Con</h2>
+
+//             </div>
+
+//             {/*toggle button*/}
+
+
+//             <div
+//                 className={
+//                     closeMenu === false
+//                         ? "burgerContainer"
+//                         : "burgerContainer active"
+//                 }
+//             >
+//                 <div
+//                     className="burgerTrigger"
+//                     onClick={() => {
+//                         handleCloseMenu();
+//                     }}
+//                 ></div>
+//                 <div className="burgerMenu"></div>
+//             </div>
+
+//             {/* profile */}
+//             <div
+//                 className={
+//                     closeMenu === false
+//                         ? "profileContainer"
+//                         : "profileContainer active"
+//                 }
+//             >
+//                 <i className="fa-solid fa-user profile icon profile"></i>
+
+//                 <div className="profileContents">
+//                     <p className="name">Hello, Rohit👋</p>
+//                     <p>neeraj@gmail.com</p>
+//                 </div>
+//             </div>
+
+
+//             <div
+//                 className={
+//                     closeMenu === false
+//                         ? "contentsContainer"
+//                         : "contentsContainer active"
+//                 }
+//             >
+//                 <ul>
+//                     {/* dashboard */}
+//                     <li className={location.pathname === "/dashboard" ? "active" : ""}>
+
+//                         <a href="/dashboard">
+//                          <i className="fa-brands fa-microsoft icon" ></i>
+//                          &nbsp;&nbsp;
+//                         dashboard</a>
+//                     </li>
+
+
+
+//                     {/* appointments */}
+//                     <li
+//                         className={
+//                             location.pathname === "/appointments"
+//                                 ? "active"
+//                                 : ""
+//                         }
+//                     >
+//                         <i className="fa-solid fa-calendar-check icon"></i>
+//                         <a href="/appointments">Appointments</a>
+//                     </li>
+
+//                     {/* Message */}
+//                     <li
+//                         className={
+//                             location.pathname === "/message" ? "active" : ""
+//                         }
+//                     >
+//                         <i className="fa-solid fa-message icon"></i>
+//                         <a href="/message">Messages</a>
+//                     </li>
+
+
+//                     {/* History */}
+//                     <li
+//                         className={
+//                             location.pathname === "/history" ? "active" : ""
+//                         }
+//                     >
+//                         <i className="fa-solid fa-book-medical icon"></i>
+//                         <a href="/history">History</a>
+//                     </li>
+
+
+//                     {/* VideoCall */}
+//                     <li
+//                         className={
+//                             location.pathname === "/connect" ? "active" : ""
+//                         }
+//                     >
+//                         <i className="fa-solid fa-headset icon"></i>
+//                         <a href="/connect">Connect</a>
+//                     </li>
+
+
+//                 </ul>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Sidebar;
+
+import './Sidebar.css';
+
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
-import '../../../styles/Sidebar.scss'
 
 const Sidebar = () => {
     const location = useLocation();
-
     const [closeMenu, setCloseMenu] = useState(false);
 
     const handleCloseMenu = () => {
@@ -13,119 +150,82 @@ const Sidebar = () => {
     };
 
     return (
-        <div className={closeMenu === false ? "sidebar w-64" : "sidebar active"}>
-            {/* logo */}
+        <>
             <div
-                className={
-                    closeMenu === false
-                        ? "logoContainer"
-                        : "logoContainer active"
-                }
+                className={`sidebar ${closeMenu ? "active" : ""}`}
             >
-                <i className="fa-brands fa-slack text-green-500 fa-lg"></i>
-                <h2 className="title ">Well-Con</h2>
-            </div>
+                {/* Logo */}
+                <div className={`logoContainer ${closeMenu ? "active" : ""}`}>
+                    <i className="fa-brands fa-slack text-green-500 fa-lg logo"></i>
+                    <h2 className="title">Well-Con</h2>
+                </div>
 
-            {/*toggle button*/}
-
-
-            <div
-                className={
-                    closeMenu === false
-                        ? "burgerContainer"
-                        : "burgerContainer active"
-                }
-            >
+                {/* Toggle */}
                 <div
-                    className="burgerTrigger"
-                    onClick={() => {
-                        handleCloseMenu();
-                    }}
-                ></div>
-                <div className="burgerMenu"></div>
-            </div>
+                    className={`burgerContainer ${closeMenu ? "active" : ""}`}
+                >
+                    <div
+                        className="burgerTrigger"
+                        onClick={handleCloseMenu}
+                    ></div>
+                    <div className="burgerMenu"></div>
+                </div>
 
-            {/* profile */}
-            <div
-                className={
-                    closeMenu === false
-                        ? "profileContainer"
-                        : "profileContainer active"
-                }
-            >
-                <i className="fa-solid fa-user profile icon"></i>
+                {/* Profile */}
+                <div className={`profileContainer ${closeMenu ? "active" : ""}`}>
+                    <i className="fa-solid fa-user profile icon profile"></i>
+                    <div className="profileContents">
+                        <p className="name">Hello, Rohit👋</p>
+                        <p>neeraj@gmail.com</p>
+                    </div>
+                </div>
 
-                <div className="profileContents">
-                    <p className="name">Hello, Rohit👋</p>
-                    <p>neeraj@gmail.com</p>
+                {/* Links */}
+                <div className={`contentsContainer ${closeMenu ? "active" : ""}`}>
+                    <ul>
+                        <li className={location.pathname === "/dashboard" ? "active" : ""}>
+                            <a href="/dashboard">
+                                <i className="fa-brands fa-microsoft icon"></i>&nbsp;&nbsp;
+                                <p className='link-text'>Dashboard</p>
+
+                            </a>
+                        </li>
+
+                        <li className={location.pathname === "/appointments" ? "active" : ""}>
+                            
+                            <a href="/appointments">
+                                <i className="fa-solid fa-calendar-check icon"></i>
+                                &nbsp;&nbsp;
+                                <p className='link-text'>Appointments</p>
+                            </a>
+                        </li>
+
+                        <li className={location.pathname === "/message" ? "active" : ""}>
+                            <a href="/message">
+                                <i className="fa-solid fa-message icon"></i>&nbsp;&nbsp;
+                                <p className='link-text'>Messages</p>
+                            </a>
+                        </li>
+
+                        <li className={location.pathname === "/history" ? "active" : ""}>
+                            <a href="/history">
+                                <i className="fa-solid fa-book-medical icon"></i>&nbsp;&nbsp;
+                                <p className='link-text'>History</p>
+                            </a>
+                        </li>
+
+                        <li className={location.pathname === "/connect" ? "active" : ""}>
+                            <a href="/connect">
+                                <i className="fa-solid fa-headset icon"></i>&nbsp;&nbsp;
+                                <p className='link-text'>Connect</p>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
 
-
-            <div
-                className={
-                    closeMenu === false
-                        ? "contentsContainer"
-                        : "contentsContainer active"
-                }
-            >
-                <ul>
-                    {/* dashboard */}
-                    <li className={location.pathname === "/dashboard" ? "active" : ""}>
-                        <i className="fa-brands fa-microsoft icon" ></i>
-                        <a href="/dashboard">dashboard</a>
-                    </li>
-
-
-
-                    {/* appointments */}
-                    <li
-                        className={
-                            location.pathname === "/appointments"
-                                ? "active"
-                                : ""
-                        }
-                    >
-                        <i className="fa-solid fa-calendar-check icon"></i>
-                        <a href="/appointments">Appointments</a>
-                    </li>
-
-                    {/* Message */}
-                    <li
-                        className={
-                            location.pathname === "/message" ? "active" : ""
-                        }
-                    >
-                        <i className="fa-solid fa-message icon"></i>
-                        <a href="/message">Messages</a>
-                    </li>
-
-
-                    {/* History */}
-                    <li
-                        className={
-                            location.pathname === "/history" ? "active" : ""
-                        }
-                    >
-                        <i className="fa-solid fa-book-medical icon"></i>
-                        <a href="/history">History</a>
-                    </li>
-
-
-                    {/* VideoCall */}
-                    <li
-                        className={
-                            location.pathname === "/connect" ? "active" : ""
-                        }
-                    >
-                        <i className="fa-solid fa-headset icon"></i>
-                        <a href="/connect">Connect</a>
-                    </li>
-
-
-                </ul>
-            </div>
-        </div>
+            
+        </>
     );
 };
 
