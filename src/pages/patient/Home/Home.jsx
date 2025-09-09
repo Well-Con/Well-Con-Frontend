b82c82f40db92cb860377c748fc4fdbd9954a146
Neeraj <neerajkaushik439@gmail.com>
2025-09-09 21:37:57 +0530

    create speciality section fro patient home page

--- src/pages/patient/Home/Home.jsx
+++ src/pages/patient/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header'
+import SpecialityMenu from './SpecialityMenu'
 
 const  PatientHome = () => {
   return (
     <div>
       <Header/>
+      <SpecialityMenu/>
     </div>
   )
 }
